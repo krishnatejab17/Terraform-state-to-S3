7c4c06aeb6c5439e415762440de630b9493f522e
Krishnatejab17 <17bdevops@gmail.com>
2025-10-16 10:57:17 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}   
